59f845ea13097fc4bdb39dfe2227f8c4e6891c9d
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2025-03-20 21:19:44 -0300

    removed unnecessary store slices and actions

--- store/store.ts
+++ store/store.ts
@@ -9,26 +9,12 @@ import { AppState } from '@/types/appState/appState';
 // Create the store with middleware
 const useStore = create<AppState>()(
   devtools((set) => ({
-    activeTab: 'locales',
-    setActiveTab: (tab) => set({ activeTab: tab }),
     token: null,
     setToken: (token) => set({ token }),
     user: {},
     setUser: (user: UserData) => set({ user }),
-    isLoggedIn: false,
-    setIsLoggedIn: (isLoggedIn) => set({ isLoggedIn }),
     isLoading: false,
     setIsLoading: (isLoading) => set({ isLoading }),
-    message: null,
-    setMessage: (message) => set({ message }),
-    error: null,
-    setError: (error) => set({ error }),
-    resetError: () => set({ error: null }),
-    authPage: 'register',
-    setAuthPage: (authPage) => set({ authPage }),
-    selectedAccountType: null,
-    setSelectedAccountType: (selectedAccountType) =>
-      set({ selectedAccountType }),
   }))
 );
 
