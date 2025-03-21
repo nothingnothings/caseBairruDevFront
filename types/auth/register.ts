238144f80eb13e26cd096de1bd469e9b49397b35
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2025-03-20 21:17:41 -0300

    removed unnecessary components and pages

--- types/auth/register.ts
+++ types/auth/register.ts
@@ -1,21 +1,7 @@
 export interface RegisterData {
   email: string;
+  name: string;
   password: string;
   confirmPassword: string;
   deviceName: string | null;
-  tabibitoName?: string;
-  companyName?: string;
-  companyIndustry?: string;
-  localeName?: string;
-  localeType?: string;
-  localeAddress?: string;
-  localeZipcode?: string;
-  localeCountry?: string;
-  localeState?: string;
-  localeCity?: string;
-  localePhone?: string;
-  localeImageUrl1?: string;
-  localeImageUrl2?: string;
-  localeWebsiteUrl?: string;
-  localeDescription?: string;
 }
--- types/stamp/card.ts
+++
@@ -1,9 +0,0 @@
-export interface StampCard {
-  id: number;
-  name: string;
-  type: string;
-  state: string;
-  city: string;
-  bonus_image_url: string;
-  has_tabistore_item: boolean;
-}
